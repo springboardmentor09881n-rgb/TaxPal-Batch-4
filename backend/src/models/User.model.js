acd9538309e11c00b9d8bbe09a8cbdcddda12b4e
Riyaz1110 <imohamedriyaz84@gmail.com>
2026-07-15 18:27:14 +0530

    Fix duplicate key error and signup CORS issue

--- backend/src/models/User.model.js
+++ backend/src/models/User.model.js
@@ -39,6 +39,8 @@ const userSchema = new mongoose.Schema({
   },
 }, {
   timestamps: true,
+  toJSON: { virtuals: true },
+  toObject: { virtuals: true }
 });
 
 module.exports = mongoose.model('User', userSchema);
